5e8a65d6135ea9ae10cf68d93a5aa08c31a53c11
yahyaallouz <yahyaallouz01@gmail.com>
2026-02-11 00:01:00 +0100

    Update repository to display languages without source code

--- frontend/src/config.js
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
-    ? "http://localhost/GestionETD/backend/api"
-    : window.location.origin + "/backend/api";
